f859e1e7bd938d3d5a0d23293c1ae2b39b5895f9
jenkins <jenkins@jenkins.com>
2023-10-19 10:29:40 +0800

    ip

---
+++ update_ip.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+def main():
+    # with open("ip.txt", "w") as file:
+    #     subprocess.run(["ipconfig"], stdout=file, text=True)
+    #     file.close()
+    os.system("cmd /c ipconfig > ip.txt")
+    os.system("git add .")
+    os.system("git commit -m ip")
+    os.system("git status")
+
+
+main()
