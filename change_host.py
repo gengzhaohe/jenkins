9217027aabd8dc78c1e59e662576ce4ae2aa2c71
jenkins <jenkins@jenkins.com>
2023-10-25 09:47:51 +0800

    remove log

--- change_host.py
+++ change_host.py
@@ -13,10 +13,8 @@ def getip():
 def main():
     os.chdir(CURRENT_DIR)
 
-    print("git pull --rebase")
     # os.system("sudo git config --system --add safe.directory /Users/huajiao/myprj/tools/jenkins")
     os.system("git pull --rebase")
-    print("git pull --rebase finish")
     ip = getip()
 
     f = open('/etc/hosts', 'r')
@@ -27,7 +25,6 @@ def main():
         for line in lines:
             if line.find('jenkins') > 0:
                 line = ip + "    jenkins\n"
-                print('ip=', ip)
             f.write(line)
     # for line in lines:
     #     if line.find('jenkins') > 0:
