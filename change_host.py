4c2ad97a2e05d8962e27dcfffcce83203cc88336
jenkins <jenkins@jenkins.com>
2023-10-27 20:15:25 +0800

    log time.

--- change_host.py
+++ change_host.py
@@ -1,5 +1,8 @@
 import os
 import pickle
+import datetime
+
+
 
 script_path = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(script_path)
@@ -12,6 +15,9 @@ def getip():
 
 def main():
     os.chdir(CURRENT_DIR)
+
+    curtime = datetime.datetime.now()
+    print(curtime)
 
     # os.system("sudo git config --system --add safe.directory /Users/huajiao/myprj/tools/jenkins")
     os.system("git pull --rebase")
